cec8d5cb9c39adab64d9e9b7f79a7a023bf3f190
elseba186 <42659385+elseba186@users.noreply.github.com>
2018-09-23 02:32:03 -0300

    initial

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+//alert("Hola mundo JS");
+//console.log/("Hola mundo JS en la consola");
+
+// DOM (Document object model)
+
+// Seleccionar objetos
+// case sensitive
+
+let element = document.querySelector(".table");
+
+console.log(element);
